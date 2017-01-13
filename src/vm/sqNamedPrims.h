83a2f68a3dded7785fed659ca8ee1d86a8b9a3d2
David T Lewis <lewis@mail.msen.com>
2017-01-13 01:42:29 +0000

    VMMaker 4.16.3
    Support very early images in which method context copy fails due to stack pointer not yet adjusted to position. For these images, disable stack limit bounds check. Based on SqueakJS implementation as described below.
    
    From: Bert Freudenberg <bert@freudenbergs.de>
    Date: Wed, 11 Jan 2017 17:10:24 +0100
    To: The general-purpose Squeak developers list <squeak-dev@lists.squeakfoundation.org>
    Subject: Re: [squeak-dev] Backward image and VM compatibility
    
    Some early images fill the context stack before advancing its stack pointer. I have a flag to allow that, it's pretty certainly used in primitive 61. Normally the VM does not allow access beyond the SP because there is garbage there (stack pops do not nil out the context slot):
    
    https://github.com/bertfreudenberg/SqueakJS/search?q=allowAccessBeyondSP
    
    But since the regular VM does not allow it, no image (except the really old ones) ever does it, so I just leave the flag enabled whenever "oldPrims" is in effect ;) Would be better if we could come up with a better way to identify these images.

--- src/vm/sqNamedPrims.h
+++ src/vm/sqNamedPrims.h
@@ -1,4 +1,4 @@
-/* Smalltalk from Squeak4.5 with VMMaker 4.16.2 translated as C source on 5 January 2017 7:36:38 pm */
+/* Smalltalk from Squeak4.5 with VMMaker 4.16.3 translated as C source on 12 January 2017 8:35:52 pm */
 /* This is an automatically generated table of all builtin modules in the VM */
 
 extern sqExport vm_exports[];
