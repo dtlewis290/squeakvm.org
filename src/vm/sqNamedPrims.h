49490c2b903b147815a8b242b8f9560f4b59ec97
David T Lewis <lewis@mail.msen.com>
2019-05-07 23:53:14 +0000

    VMMaker 4.16.6 Primitive support for snapshot objects, answering object memory and header as in-image objects instead of disk-based snapshot file.
    The primitives are:
      #primitiveInterpreterStateSnapshot - snapshot the header information
      #primitiveMemoryCopy - snapshot the memory
      #primitiveMemorySnapshotBytesWithHeader - snapshot memory bytes and header
      #primitiveMemorySnapshotWithHeader - snapshot memory bitmap and header
    
    Image side support: Package ImageFormat in http://source.squeak.org/VMMaker
    and package ImageSnapshot in http://www.squeaksource.com/ImageSnapshot.
    
    Note - Changes for these primitives were inadvertently included in the
    last commit in addition to its intended change to an error warning message.
    This commit notice thus pertains to the changes in r3771 and r3772.

--- src/vm/sqNamedPrims.h
+++ src/vm/sqNamedPrims.h
@@ -1,4 +1,4 @@
-/* Smalltalk from Squeak4.6 with VMMaker 4.16.6 translated as C source on 3 May 2019 7:43:32 pm */
+/* Smalltalk from Squeak4.6 with VMMaker 4.16.6 translated as C source on 7 May 2019 7:31:01 pm */
 /* This is an automatically generated table of all builtin modules in the VM */
 
 extern sqExport vm_exports[];
