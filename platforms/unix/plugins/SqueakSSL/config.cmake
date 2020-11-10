748f78a30313be0aa1d5951e723d7c576f48fde6
David T Lewis <lewis@mail.msen.com>
2020-11-10 22:25:35 +0000

    Overlay OpenSSL for linux/unix by Tobias Pape and others Adopted from http://squeakvm.org/svn/squeak/branches/Cog
    Primary commit notices:
    
    commit 49353ed2783531b3c8fcb7a522dc6f578ea5414c
    Author: Tobias Pape <tobias@netshed.de>
    Date:   Tue Jan 30 20:09:38 2018 +0100
    
        [SqueakSSL] Overlay OpenSSL for linux/unix
        
        When we dynamically link against OpenSSL, the bundles are not portable,
        as CentOS and friends use other SO_NAMEs than Debian and friends. Also,
        soft-fallback for later features such as host name verification is hard.
        
        When we statically link, we might lack behind the OS, the binaries are
        bigger, and the legal situation is less clear.
        
        So we now support not linking at all but rather lookup all necessary
        functions/symbols at runtime.
        
        This can be disabled with SQSSL_OPENSSL_LINKED which effectively results
        in the dynamically-linked behavior. (This is preferable for platform
        builds, eg, debs and rpms)
    
    commit 7046a62348f2b128fecf4c6697750a9f11af02d6
    Author: Eliot Miranda <eliot.miranda@gmail.com>
    Date:   Thu Jul 30 01:55:26 2015 +0000
    
        Integrate Marcel Taeumel & Tobias Pape's v2 SSL plugin changes.
        
        
        git-svn-id: http://squeakvm.org/svn/squeak/branches/Cog@3417 fa1542d4-bde8-0310-ad64-8ed1123d492a
        
        Former-commit-id: 1bfa721af16c4a67f32c8aec4d2cdf9e23f29586

--- platforms/unix/plugins/SqueakSSL/config.cmake
+++ platforms/unix/plugins/SqueakSSL/config.cmake
@@ -1,1 +1,44 @@
+#
+# For platform builds, use
+#
+# PLUGIN_REQUIRE_PACKAGE (OPENSSL openssl)
+# add_definitions(-DSQSSL_OPENSSL_LINKED) 
+#
+# otherwise the OpenSSL libs will be looked up at runtime.
+
 PLUGIN_REQUIRE_PACKAGE (OPENSSL openssl)
+
+# Traditional dynamic linking.
+#
+# PLUGIN_DEFINITIONS("-DSQSSL_OPENSSL_LINKED")
+
+# Runtime symbol and function lookup.
+# Portable across platforms because only required symbols are looked up.
+# Requires a Gnu extension for implementation of symbol lookup.
+#
+PLUGIN_DEFINITIONS("-D_GNU_SOURCE")
+
+################################################################################
+#            Explanation from commit notice on opensmalltalk-vm:               #
+################################################################################
+#
+#commit 49353ed2783531b3c8fcb7a522dc6f578ea5414c
+#Author: Tobias Pape <tobias@netshed.de>
+#Date:   Tue Jan 30 20:09:38 2018 +0100
+#
+#    [SqueakSSL] Overlay OpenSSL for linux/unix
+#    
+#    When we dynamically link against OpenSSL, the bundles are not portable,
+#    as CentOS and friends use other SO_NAMEs than Debian and friends. Also,
+#    soft-fallback for later features such as host name verification is hard.
+#    
+#    When we statically link, we might lack behind the OS, the binaries are
+#    bigger, and the legal situation is less clear.
+#    
+#    So we now support not linking at all but rather lookup all necessary
+#    functions/symbols at runtime.
+#    
+#    This can be disabled with SQSSL_OPENSSL_LINKED which effectively results
+#    in the dynamically-linked behavior. (This is preferable for platform
+#    builds, eg, debs and rpms)
+
