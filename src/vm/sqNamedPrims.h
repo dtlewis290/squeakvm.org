0eccaabaad599b98b599ed85d011486fff3f2aa6
David T Lewis <lewis@mail.msen.com>
2025-08-09 15:39:58 +0000

    Update sources per VMMaker-dtl.445 Add primitiveBytecodeSetsAvailable referenced from newer Squeak images (see www.squeaksource.com/TrunkUpdateStreamV3)

--- src/vm/sqNamedPrims.h
+++ src/vm/sqNamedPrims.h
@@ -1,4 +1,4 @@
-/* Smalltalk from Squeak4.6 with VMMaker 4.20.10 translated as C source on 7 June 2024 5:48:01 pm */
+/* Smalltalk from Squeak4.6 with VMMaker 4.20.11 translated as C source on 9 August 2025 9:57:01 am */
 /* This is an automatically generated table of all builtin modules in the VM */
 
 extern sqExport vm_exports[];
